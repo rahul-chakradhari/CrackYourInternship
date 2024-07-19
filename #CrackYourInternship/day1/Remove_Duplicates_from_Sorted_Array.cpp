82e9eda5da2a40991f693dcfc733d6a56193cacd
Rahul Chakradhari <96400194+rahul-chakradhari@users.noreply.github.com>
2024-07-19 23:05:24 +0530

    Add files via upload

---
+++ #CrackYourInternship/day1/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.empty())
+        {
+            return 0;
+        }
+
+        int uniqueCount = 1;
+        int n = nums.size();
+
+        for (int i = 1; i < n; ++i)
+        {
+
+            if (nums[i] != nums[i - 1])
+            {
+
+                nums[uniqueCount] = nums[i];
+
+                uniqueCount++;
+            }
+        }
+
+        return uniqueCount;
+    }
+};
